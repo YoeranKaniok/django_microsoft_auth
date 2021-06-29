3cae615102040a7deda787cfe03a36c32739de4a
yka <yka@iton.nl>
2021-06-29 09:36:57 +0200

    prefered username instead of upn

--- microsoft_auth/backends.py
+++ microsoft_auth/backends.py
@@ -173,7 +173,7 @@ class MicrosoftAuthenticationBackend(ModelBackend):
 
             try:
                 # create new Django user from provided data
-                user = User.objects.get(email=data["upn"])
+                user = User.objects.get(email=data["preferred_username"])
 
                 if user.first_name == "" and user.last_name == "":
                     user.first_name = first_name
@@ -184,7 +184,7 @@ class MicrosoftAuthenticationBackend(ModelBackend):
                     username=data["preferred_username"][:150],
                     first_name=first_name,
                     last_name=last_name,
-                    email=data["upn"],
+                    email=data["preferred_username"],
                 )
                 user.save()
 
